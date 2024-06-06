cf1db00ebf7edb77ad02ac8fbeb53312a9103ab5
IArchi <yoann.hamon@orange.com>
2024-06-06 21:02:10 +0200

    Add ledring tests.

---
+++ test/test_ringled.py
@@ -0,0 +1,25 @@
+import time
+
+sys.path.append('..')
+from libs.ringled import RingLed
+
+leds = RingLed()
+
+print('Start countdown')
+leds.start_countdown(5)
+time.sleep(5)
+
+print('Start rainbow')
+leds.start_rainbow()
+time.sleep(5)
+
+print('Start flash')
+leds.flash()
+time.sleep(5)
+
+print('Start blink')
+leds.blink((1,0,0,1))
+time.sleep(5)
+
+print('Stop')
+leds.stop()
