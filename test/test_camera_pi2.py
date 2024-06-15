926a0ab33bd8f96a5adbf5602d443b3ea1ac05d0
IArchi <y8hamon@gmail.com>
2024-06-15 09:56:38 +0200

    Fix test.

--- test/test_camera_pi2.py
+++ test/test_camera_pi2.py
@@ -1,41 +1,8 @@
 import sys
+import cv2
 
 sys.path.append('..')
 from libs.device_utils import Picamera2Camera
-
-# The following code works with Camera module v3
-import cv2
-import time
-from picamera2 import Picamera2
-from libcamera import controls
-
-
-picam2 = Picamera2()
-
-preview_config = picam2.create_preview_configuration(main={'format': 'RGB888', 'size': (1920, 1080)}, controls={'FrameRate': 30})
-still_config = picam2.create_still_configuration(main={"size": (1920, 1080), "format": "RGB888"}, buffer_count=1, controls={'FrameRate': 30})
-picam2.configure(preview_config)
-
-def print_af_state(request):
-    md = request.get_metadata()
-    print(("Idle", "Scanning", "Success", "Fail")[md['AfState']], md.get('LensPosition'))
-
-picam2.pre_callback = print_af_state
-picam2.start()
-print('Started')
-picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous,"AfSpeed": controls.AfSpeedEnum.Fast})
-start_time = time.time()
-job = picam2.autofocus_cycle(wait=False)
-
-for i in range(0, 100):
-    if job.get_result():
-        print(f'Autofocused: {time.time() - start_time}s')
-        picam2.switch_mode_and_capture_file(still_config, 'test.jpg')
-        picam2.stop()
-        break
-    time.sleep(0.1)
-
-
 
 # # Connect to camera
 camera = Picamera2Camera(port=0)
