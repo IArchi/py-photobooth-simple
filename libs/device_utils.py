fea5b36f8135fb4b0f590b13c4814f0522b7820d
IArchi <yoann.hamon@orange.com>
2024-06-18 15:55:26 +0200

    Fix print task.

--- libs/device_utils.py
+++ libs/device_utils.py
@@ -222,7 +222,7 @@ class Picamera2Camera(CaptureDevice):
         self._resize(output_name, im)
 
 class CupsPrinter(PrintDevice):
-    _name = None
+    _printer = None
 
     def __init__(self, name=None):
         if cups:
@@ -237,7 +237,7 @@ class CupsPrinter(PrintDevice):
 
             # Cannot find any printer
             if printer_found:
-                self._name = name
+                self._printer = printer_found
                 self._instance = cups_conn
                 Logger.info('CupsPrinter: Connected to printer \'%s\'', name)
             elif name.lower() == 'default':
@@ -247,7 +247,7 @@ class CupsPrinter(PrintDevice):
         if not self._instance: raise Exception('Cannot find any CUPS printer or cups is not installed.')
 
     def print(self, file_path, print_params={}):
-        return self._instance.printFile(self._name, file_path, ' ', print_params)
+        return self._instance.printFile(self._printer, file_path, ' ', print_params)
 
     def get_print_status(self, task_id):
         status = self._instance.getJobAttributes(task_id)['job-state']
