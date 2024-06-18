7ee8b62c9c4abaac1a20735e4d689cb50aee3aee
IArchi <y8hamon@gmail.com>
2024-06-18 14:13:21 +0200

    Set manual mode.

--- test/test_gphoto2.py
+++ test/test_gphoto2.py
@@ -9,25 +9,27 @@ if gp.cameraList().count():
     _instance = gp.camera()
 
     # Print summary
-    #print(_instance.summary())
+    print(_instance.summary())
 
     # Retrieve settings (For EOS 2000D: https://github.com/gphoto/libgphoto2/blob/master/camlibs/ptp2/cameras/canon-eos2000d.txt)
     config = _instance.get_config()
 
     # Print avilable settings
-    #print('Available parameters:')
-    #print("\n".join(config.list_paths()))
+    print('Available parameters:')
+    print("\n".join(config.list_paths()))
 
     # Print some
     print('/main/capturesettings/shutterspeed', config.get_path('/main/capturesettings/shutterspeed').get_value())
     print('/main/imgsettings/iso', config.get_path('/main/imgsettings/iso').get_value())
 
     # Update some
+    config.get_path('/main/capturesettings/autoexposuremode').set_value('Manual')
+
     #config.get_path('/main/actions/viewfinder').set_value(1) # Autofocus (If it does not work, use main.actions.eosremoterelease)
     config.get_path('/main/capturesettings/focusmode').set_value('Manual')
     config.get_path('/main/capturesettings/focusmode').set_value('One Shot')
     config.get_path('/main/capturesettings/aperture').set_value('8')
-    #config.get_path('/main/capturesettings/shutterspeed').set_value('1/160')
+    config.get_path('/main/capturesettings/shutterspeed').set_value('1/160')
     config.get_path('/main/imgsettings/iso').set_value('400')
 
     # Commit changes
