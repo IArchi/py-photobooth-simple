e41c5e79b38ff746e862d0e71e3c650ca4ed801a
IArchi <yoann.hamon@orange.com>
2026-07-14 15:58:48 +0200

    Add thread lock for ringled.

--- libs/ringled.py
+++ libs/ringled.py
@@ -16,40 +16,46 @@ class RingLed:
         Logger.info('RingLed: __init__().')
         self._num_pixels = num_pixels
         self._top_pixel = 6
+        self._proc = None
+        self._stop = threading.Event()
+        self._lock = threading.Lock()
+        self._leds = None
         if spidev is None: return
         spi = spidev.SpiDev()
         spi.open(0,0)
-        self._proc = None
-        self._stop = threading.Event()
         self._leds = WS2812(spi, self._num_pixels)
+
+    def _stop_worker(self):
+        if self._proc and self._proc.is_alive():
+            self._stop.set()
+            self._proc.join(timeout=1)
+            if self._proc.is_alive():
+                Logger.warning('RingLed: worker thread did not stop cleanly')
+        self._proc = None
+
+    def _start_worker(self, name, target, *args):
+        with self._lock:
+            self._stop_worker()
+            self._stop.clear()
+            self._proc = threading.Thread(target=target, args=args, name=f'ringled-{name}', daemon=True)
+            self._proc.start()
 
     def start_countdown(self, time_seconds):
         Logger.info('RingLed: start_countdown().')
         if spidev is None: return
-        if self._proc and self._proc.is_alive():
-            self._stop.set()
-            self._proc.join()
-        self._stop.clear()
-        self._proc = threading.Thread(target=self._countdown, args=[time_seconds])
-        self._proc.start()
+        self._start_worker('countdown', self._countdown, time_seconds)
 
     def start_rainbow(self):
         Logger.info('RingLed: start_rainbow().')
         if spidev is None: return
-        if self._proc and self._proc.is_alive():
-            self._stop.set()
-            self._proc.join()
-        self._stop.clear()
-        self._proc = threading.Thread(target=self._rainbow)
-        self._proc.start()
+        self._start_worker('rainbow', self._rainbow)
 
     def flash(self, stop=False):
         Logger.info('RingLed: flash().')
         if spidev is None: return
-        if self._proc and self._proc.is_alive():
-            self._stop.set()
-            self._proc.join()
-        self._stop.clear()
+        with self._lock:
+            self._stop_worker()
+            self._stop.clear()
         if not stop:
             self._leds.fill([255,255,255])
             time.sleep(0.1)
@@ -61,31 +67,20 @@ class RingLed:
     def blink(self, color):
         Logger.info('RingLed: blink().')
         if spidev is None: return
-        if self._proc and self._proc.is_alive():
-            self._stop.set()
-            self._proc.join()
-        self._stop.clear()
-        self._proc = threading.Thread(target=self._blink, args=[color,])
-        self._proc.start()
+        self._start_worker('blink', self._blink, color)
 
     def wave(self, color):
         Logger.info('RingLed: wave().')
         if spidev is None: return
-        if self._proc and self._proc.is_alive():
-            self._stop.set()
-            self._proc.join()
-        self._stop.clear()
-        self._proc = threading.Thread(target=self._wave, args=[color,])
-        self._proc.start()
+        self._start_worker('wave', self._wave, color)
 
     def clear(self):
         Logger.info('RingLed: clear().')
         if spidev is None: return
-        if self._proc and self._proc.is_alive():
-            self._stop.set()
-            self._proc.join()
-        self._stop.clear()
-        self._leds.fill([0,0,0])
+        with self._lock:
+            self._stop_worker()
+            self._stop.clear()
+            self._leds.fill([0,0,0])
 
     def _blink(self, color):
         while True:
@@ -93,7 +88,7 @@ class RingLed:
             time.sleep(0.1)
             self._leds.fill([0,0,0])
             time.sleep(0.1)
-            if self._stop.isSet(): return
+            if self._stop.is_set(): return
 
     def _wave(self, color):
         wave_intensity = 0.5
@@ -105,7 +100,7 @@ class RingLed:
                     intensity = (math.sin(2 * math.pi * ((i + step) % wave_length) / wave_length) + 1) / 2
                     intensity = wave_intensity * intensity  # Scale intensity to desired range
                     self._leds.set(i, color, brightness=intensity)
-                    if self._stop.isSet(): return
+                    if self._stop.is_set(): return
                 time.sleep(0.1)
 
     def _countdown(self, time_seconds):
@@ -118,7 +113,7 @@ class RingLed:
         for i in [*p1, *p2]:
             self._leds.set(i, [0,0,0])
             time.sleep(time_between_pixels)
-            if self._stop.isSet(): return
+            if self._stop.is_set(): return
 
     def _rainbow(self):
         hue_step = 1.0 / self._num_pixels
@@ -133,5 +128,5 @@ class RingLed:
                     # Scale the RGB values to 0-255
                     rgb_scaled = [int(255 * x) for x in rgb]
                     self._leds.set(i, rgb_scaled)
-                    if self._stop.isSet(): return
+                    if self._stop.is_set(): return
                 time.sleep(0.1)
