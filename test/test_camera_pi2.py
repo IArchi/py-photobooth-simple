09791904dfbcdb27c8023ae0eb9f14cc7c32c6f0
IArchi <y8hamon@gmail.com>
2026-07-16 15:18:31 +0200

    Add FPS + resoltuion in tests.

--- test/test_camera_pi2.py
+++ test/test_camera_pi2.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import cv2
 
 sys.path.append('..')
@@ -6,12 +7,27 @@ from libs.device_utils import Picamera2Camera
 
 # # Connect to camera
 camera = Picamera2Camera(port=0)
+last_frame_at = None
+fps = None
 while True:
     im = camera.get_preview()
-    if im is None: continue
-    camera.cv2_imshow(im)
+    if im is None:
+        continue
+
+    now = time.perf_counter()
+    if last_frame_at is not None:
+        instant_fps = 1.0 / max(now - last_frame_at, 1e-6)
+        fps = instant_fps if fps is None else (fps * 0.9 + instant_fps * 0.1)
+    last_frame_at = now
+
+    height, width = im.shape[:2]
+    overlay = f'{width}x{height} | {fps:.1f} FPS' if fps is not None else f'{width}x{height} | measuring FPS'
+    display = cv2.flip(im, 0)
+    cv2.putText(display, overlay, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.imshow('Camera', display)
     if cv2.waitKey(1) > 0: break
 
 # Trigger capture
 camera.capture('test.jpg')
+camera.close()
 cv2.destroyAllWindows()
--- test/test_gphoto2.py
+++ test/test_gphoto2.py
@@ -1,3 +1,4 @@
+import os
 import sys
 sys.path.append('..')
 import cv2
@@ -15,94 +16,108 @@ def signal_handler(sig, frame):
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
-if gp.cameraList().count():
-    _instance = gp.camera()
+if not gp.cameraList().count():
+    raise SystemExit('Cannot find any gPhoto2 camera.')
 
-    try:
+_instance = gp.camera()
+tmp_output = None
 
-        # Print summary
-        print(_instance.summary())
+try:
+    # Print summary
+    print(_instance.summary())
 
-        # Retrieve settings (For EOS 2000D: https://github.com/gphoto/libgphoto2/blob/master/camlibs/ptp2/cameras/canon-eos2000d.txt)
-        config = _instance.get_config()
+    # Retrieve settings (For EOS 2000D: https://github.com/gphoto/libgphoto2/blob/master/camlibs/ptp2/cameras/canon-eos2000d.txt)
+    config = _instance.get_config()
 
-        # Print avilable settings
-        print('Available parameters:')
-        print("\n".join(config.list_paths()))
+    # Print avilable settings
+    print('Available parameters:')
+    print("\n".join(config.list_paths()))
 
-        # Print some
-        print('/main/capturesettings/shutterspeed', config.get_path('/main/capturesettings/shutterspeed').get_value())
-        print('/main/imgsettings/iso', config.get_path('/main/imgsettings/iso').get_value())
+    # Print some
+    print('/main/capturesettings/shutterspeed', config.get_path('/main/capturesettings/shutterspeed').get_value())
+    print('/main/imgsettings/iso', config.get_path('/main/imgsettings/iso').get_value())
 
-        # Update some
-        manufacturer = config.get_path('/main/status/manufacturer').get_value()
-        match manufacturer:
-            case 'Canon Inc.':
-                # From https://github.com/gphoto/libgphoto2/blob/master/camlibs/ptp2/cameras/canon-eos2000d.txt
-                current_mode = config.get_path('/main/capturesettings/autoexposuremode').get_value()
-                if current_mode in ['Manual', 'TV']:
-                    config.get_path('/main/capturesettings/shutterspeed').set_value('1/125')
-                if current_mode in ['Manual', 'AV']:
-                    config.get_path('/main/capturesettings/aperture').set_value('13')
-                    config.get_path('/main/capturesettings/focusmode').set_value('One Shot')
-                    config.get_path('/main/imgsettings/iso').set_value('100')
-                break
+    # Update some
+    manufacturer = config.get_path('/main/status/manufacturer').get_value()
+    if manufacturer == 'Canon Inc.':
+        # From https://github.com/gphoto/libgphoto2/blob/master/camlibs/ptp2/cameras/canon-eos2000d.txt
+        current_mode = config.get_path('/main/capturesettings/autoexposuremode').get_value()
+        if current_mode in ['Manual', 'TV']:
+            config.get_path('/main/capturesettings/shutterspeed').set_value('1/125')
+        if current_mode in ['Manual', 'AV']:
+            config.get_path('/main/capturesettings/aperture').set_value('13')
+        config.get_path('/main/capturesettings/focusmode').set_value('One Shot')
+        config.get_path('/main/imgsettings/iso').set_value('100')
+    elif manufacturer == 'Nikon Corporation':
+        # From https://github.com/gphoto/libgphoto2/blob/master/camlibs/ptp2/cameras/nikon-z6.txt
+        current_mode = config.get_path('/main/capturesettings/expprogram').get_value()
+        if current_mode in ['M', 'S']:
+            config.get_path('/main/capturesettings/shutterspeed').set_value('1/125')
+        if current_mode in ['M', 'A']:
+            config.get_path('/main/capturesettings/f-number').set_value('f/13')
+        config.get_path('/main/capturesettings/focusmode').set_value('AF-S')
+        config.get_path('/main/imgsettings/iso').set_value('100')
+    elif manufacturer == 'Sony Corporation':
+        # From https://github.com/gphoto/libgphoto2/blob/master/camlibs/ptp2/cameras/sony-a7c.txt
+        current_mode = config.get_path('/main/capturesettings/expprogram').get_value()
+        if current_mode in ['M', 'S']:
+            config.get_path('/main/capturesettings/shutterspeed').set_value('1/125')
+        if current_mode in ['M', 'A']:
+            config.get_path('/main/capturesettings/f-number').set_value('f/13')
+        config.get_path('/main/capturesettings/focusmode').set_value('AF-A')
+        config.get_path('/main/imgsettings/iso').set_value('100')
+    else:
+        print(f'Unsupported camera model: {manufacturer}')
 
-            case 'Nikon Corporation':
-                # From https://github.com/gphoto/libgphoto2/blob/master/camlibs/ptp2/cameras/nikon-z6.txt
-                current_mode = config.get_path('/main/capturesettings/expprogram').get_value()
-                if current_mode in ['M', 'S']:
-                    config.get_path('/main/capturesettings/shutterspeed').set_value('1/125')
-                if current_mode in ['M', 'A']:
-                    config.get_path('/main/capturesettings/f-number').set_value('f/13')
-                config.get_path('/main/capturesettings/focusmode').set_value('AF-S')
-                config.get_path('/main/imgsettings/iso').set_value('100')
-                break
+    # Commit changes
+    _instance.commit_config(config)
 
-            case 'Sony Corporation':
-                # From https://github.com/gphoto/libgphoto2/blob/master/camlibs/ptp2/cameras/sony-a7c.txt
-                current_mode = config.get_path('/main/capturesettings/expprogram').get_value()
-                if current_mode in ['M', 'S']:
-                    config.get_path('/main/capturesettings/shutterspeed').set_value('1/125')
-                if current_mode in ['M', 'A']:
-                    config.get_path('/main/capturesettings/f-number').set_value('f/13')
-                config.get_path('/main/capturesettings/focusmode').set_value('AF-A')
-                config.get_path('/main/imgsettings/iso').set_value('100')
-                break
+    # Trigger capture to focus
+    _instance.capture_image()
 
-            case _:
-                Logger.info('Unsupported camera model: %s', manufacturer)
-                return
+    # Display preview
+    PREVIEW_TO_FILE = False
+    fd, tmp_output = tempfile.mkstemp(suffix='.jpg')
+    os.close(fd)
+    last_frame_at = None
+    fps = None
+    while True:
+        if PREVIEW_TO_FILE:
+            # Capture is saved into a local file
+            _instance.capture_preview(tmp_output)
+            im = cv2.imread(tmp_output)
+        else:
+            # Capture is directly read as bytes array
+            cfile = _instance.capture_preview()
+            buf = np.frombuffer(cfile.get_data(), dtype=np.uint8)
+            im = cv2.imdecode(buf, cv2.IMREAD_COLOR)
 
-        # Commit changes
-        _instance.commit_config(config)
+        if im is None:
+            continue
 
-        # Trigger capture to focus
-        _instance.capture_image()
+        now = time.perf_counter()
+        if last_frame_at is not None:
+            instant_fps = 1.0 / max(now - last_frame_at, 1e-6)
+            fps = instant_fps if fps is None else (fps * 0.9 + instant_fps * 0.1)
+        last_frame_at = now
 
-        # Display preview
-        PREVIEW_TO_FILE = False
-        _, tmp_output = tempfile.mkstemp(suffix='.jpg')
-        while True:
-            if PREVIEW_TO_FILE:
-                # Capture is saved into a local file
-                _instance.capture_preview(tmp_output)
-                im = cv2.imread(tmp_output)
-            else:
-                # Capture is directly read as bytes array
-                cfile = _instance.capture_preview()
-                buf = np.frombuffer(cfile.get_data(), dtype=np.uint8)
-                im = cv2.imdecode(buf, cv2.IMREAD_COLOR)
+        height, width = im.shape[:2]
+        overlay = f'{width}x{height} | {fps:.1f} FPS' if fps is not None else f'{width}x{height} | measuring FPS'
+        display = im.copy()
+        cv2.putText(display, overlay, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.imshow('Camera', display)
+        if cv2.waitKey(1) > 0:
+            break
 
-            # Display
-            cv2.imshow('Camera', im)
-            if cv2.waitKey(1) > 0: break
+    # Trigger capture (Can also be captured as a bytes array)
+    _instance.capture_image('./capture.jpg')
 
-        # Trigger capture (Can also be captured as a bytes array)
-        _instance.capture_image('./capture.jpg')
-    
-    catch Exception as e:
-        print(e)
+except Exception as e:
+    print(e)
 
-    # Release camera
-    _instance.close()
+finally:
+    if _instance:
+        _instance.close()
+    if tmp_output and os.path.exists(tmp_output):
+        os.remove(tmp_output)
+    cv2.destroyAllWindows()
