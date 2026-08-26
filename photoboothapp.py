30a3e873d5bbad99b1494ab491abff9be235b78e
IArchi <y8hamon@gmail.com>
2026-08-26 11:13:15 +0200

    Change log level.

--- photoboothapp.py
+++ photoboothapp.py
@@ -16,6 +16,7 @@ LOG_DIRECTORY.mkdir(exist_ok=True)
 
 from kivy.config import Config as KivyConfig
 KivyConfig.set('kivy', 'log_enable', '1')
+KivyConfig.set('kivy', 'log_level', 'info')
 KivyConfig.set('kivy', 'log_dir', str(LOG_DIRECTORY))
 KivyConfig.set('kivy', 'log_name', 'photobooth_%y-%m-%d_%_.txt')
 KivyConfig.set('kivy', 'exit_on_escape', '0')
